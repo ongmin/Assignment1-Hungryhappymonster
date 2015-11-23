9f06ddd0c70f3a874aecd4dea15ced4e5a45b1bd
Min Ong <xinmin.ong@gmail.com>
2015-11-23 23:09:55 +0800

    Updated to object-oriented programming

--- game/game.js
+++ game/game.js
@@ -1,6 +1,6 @@
 /* global Image */
 
-var canvas = document.querySelector('#gamecanvas1')
+var canvas = document.querySelector('#gamecanvas')
 var context = canvas.getContext('2d')
 
 var maxX = canvas.width
@@ -9,67 +9,55 @@ var maxY = canvas.height
 var minX = 0
 var minY = 0
 
-var actors = []
+var allArray = []
 
-var monsterImage1 = new Image()
-monsterImage1.src = 'images/spikymonster1_small.png'
-var monsterImage2 = new Image()
-monsterImage2.src = 'images/spikymonster2_small.png'
+var token = new Image()
+token.src = 'images/coin_small.png'
 
-class Monster {
-  constructor (x, y, image = monsterImage1) {
-    this.x = maxX - this.width
-    this.y = maxY - this.height
+var player = new Image()
+player.src = 'images/spikymonster1_left_small.png'
+
+class Token {
+  constructor (x, y, image = token) {
+    this.x = x
+    this.y = y
     this.image = image
-    this.width = image.width
-    this.height = image.height
-  }
-  render () {
-    var imageFrame = 1
-    if (imageFrame === 1) {
-      context.drawImage(this.image, this.x, this.y)
-      imageFrame = 2
-    } else {
-      context.drawImage(monsterImage2, this.x, this.y)
-      imageFrame = 1
-    }
-  }
-}
-
-var tokenImage1 = new Image()
-var tokenImage2 = new Image()
-tokenImage1.src = 'images/coin_small.png'
-tokenImage2.src = 'images/coin_small.png'
-
-class Token extends Monster {
-  constructor (x, y, image = tokenImage1) {
-    super(x, y, image)
   }
   render () {
     context.drawImage(this.image, this.x, this.y)
   }
 }
 
-function setup () {
-  for (var i = 0; i < 10; i++) {
-    var x = (Math.random() * ((maxX-50)-minX)) + minX
-    var y = (Math.random() * ((maxY-50)-minY)) + minY
-    var token = new Token(x, y)
-    actors.push(token)
+class Player {
+  constructor (x, y, image = player) {
+    this.x = x
+    this.y = y
+    this.image = image
   }
-  var monster = new Monster(0, 0)
-  actors.push(monster)
+  render () {
+    context.drawImage(this.image, this.x, this.y)
+    // context.drawImage(this.image, this.x + 10, this.y + 10)
+  }
 }
 
-function render () {
+function setup () {
+  for (var i = 0; i < 10; i++) {
+    var x = (Math.random() * ((maxX - 50) - minX)) + minX
+    var y = (Math.random() * ((maxY - 50) - minY)) + minY
+    var token = new Token(x, y)
+    allArray.push(token)
+  }
+
+  var player = new Player(10, 10)
+  allArray.push(player)
+}
+
+function renderAll () {
   context.clearRect(0, 0, canvas.width, canvas.height)
-  actors.forEach(monster => {
-    monster.render()
-  })
-  actors.forEach(token => {
-    token.render()
+  allArray.forEach(character => {
+    character.render()
   })
 }
 
 setup()
-setInterval(render, 16.6667)
+setInterval(renderAll, 1000)
